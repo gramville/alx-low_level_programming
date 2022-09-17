3e41921d72fe9dc3a5a09e0264a025dbc8324a9d
gramville <111007717+gramville@users.noreply.github.com>
2022-09-17 23:29:35 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,4 @@
-/*
- * File: 0-holberton.c
- * Auth: Brennan D Baraban
- *
- */
-
 #include "main.h"
-
-/**
- * main - Prints "Holberton" followed by a new line.
- *
- * Return: Always 0.
- */
 
 int main(void)
 {

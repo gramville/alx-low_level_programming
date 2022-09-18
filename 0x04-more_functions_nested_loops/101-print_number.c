d1ec1fe07a02d71def17c56a1d3a27fd87362569
gramville <111007717+gramville@users.noreply.github.com>
2022-09-18 21:33:34 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,33 +8,17 @@
 
 void print_number(int n)
 {
-	long len, res, i, temp, expo;
 
-	res = n;
-	expo = len =  1;
-/*Check negatives*/
-	if (res < 0)
-	{
-		res *= -1;
-		_putchar('-');
-	}
+    if (n < 0) {
+        putchar('-');
+        n = -n;
+    }
 
-/**/
-	temp = res;
-	while (temp >= 10)
-	{
-		len++;
-		temp /= 10;
-	}
+    if (n == 0)
+        putchar('0');
 
-/*Create Exponent*/
-	for (i = 1; i < len; i++)
-		expo *= 10;
-/*Main */
-	while (expo > 1)
-	{
-		_putchar((res / expo) % 10 + '0');
-		expo /= 10;
-	}
-	_putchar(res % 10 + '0');
+    if (n/10)
+        printnumber(n/10);
+
+    putchar(n%10 + '0');
 }

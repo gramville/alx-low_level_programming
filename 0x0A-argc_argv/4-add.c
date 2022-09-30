3029114a9a72e2895341757b454e86d26c5c83b0
gramville <111007717+gramville@users.noreply.github.com>
2022-09-30 22:10:32 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,41 +1,30 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
-#include <string.h>
+
 /**
- * checker - checks for valid input
+ * main - adds all positive numbers and prints it
  * @argc: argument count
- * @i: counter for argv[]
- * @j: counter for argv[][]
  * @argv: argument vector
- * Return: 0 on success, 1 on failure
+ * Return: 0 for success
  */
-int checker(int argc, int i, unsigned int j, char *argv[])
-{
-	for (i = 1; i <= argc; i++)
-		for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
-			if (isdigit(argv[i][j]) == 0)
-				return (1);
-	return (0);
-}
-/**
- * main - adds all arguments together if they are digits.
- * @argc: argument count only accepts ints separated by spaces.
- * @argv: argument vector
- * Return: 0 on success, 1 on failure
- */
+
 int main(int argc, char *argv[])
 {
-	int result, i;
+	int i, res;
 
-	result = 0;
-	if (checker(argc, 1, 0, argv) == 1)
+	res = 0;
+
+	for (i = 0; i < argc; i++)
 	{
-		printf("Error\n");
-		return (1);
+		if (i > 0)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				return (printf("Error\n"), 1);
+			}
+			res += atoi(argv[i]);
+		}
 	}
-	for (i = 1; i < argc; i++)
-		result += atoi(argv[i]);
-	printf("%d\n", result);
+	printf("%d\n", res);
 	return (0);
 }
